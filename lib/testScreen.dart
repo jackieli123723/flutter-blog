f05ed06216cc0b1f30bc0412641b7199927d1f2f
jackieli123723 <380863274@qq.com>
2020-02-17 16:19:20 +0800

    tab-bar

---
+++ lib/testScreen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_point_tab_bar/pointTabIndicator.dart';
+
+class TestScreen extends StatefulWidget {
+  @override
+  _TestScreenState createState() => _TestScreenState();
+}
+
+class _TestScreenState extends State<TestScreen>
+    with SingleTickerProviderStateMixin {
+  final tabList = ['前端', '后端'];
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(vsync: this, length: tabList.length);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('西门互联'),
+        centerTitle: true,
+        bottom: TabBar(
+          controller: _tabController,
+          indicator: PointTabIndicator(
+            position: PointTabIndicatorPosition.bottom,
+            color: Colors.white,
+            insets: EdgeInsets.only(bottom: 6),
+          ),
+          tabs: tabList.map((item) {
+            return Tab(
+              text: item,
+            );
+          }).toList(),
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: tabList.map((item) {
+          return Center(child: Text(item));
+        }).toList(),
+      ),
+    );
+  }
+}
