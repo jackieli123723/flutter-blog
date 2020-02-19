d22128266b64f7819eee7d71c3e579c7a814a604
jackieli123723 <380863274@qq.com>
2020-02-19 17:41:22 +0800

    更新 截图

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '西门互联',
+      title: '西门互联1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
