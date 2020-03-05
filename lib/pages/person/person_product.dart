5f01ee7f03636c57f89b5b4947102c127f33a839
jackieli123723 <380863274@qq.com>
2020-03-05 10:13:21 +0800

    backup

--- lib/pages/person/person_product.dart
+++ lib/pages/person/person_product.dart
@@ -1,10 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-String vueUrl = 'http://iph.href.lu/50x50?text=v&fg=fff&bg=000';
-String nodeUrl = 'http://iph.href.lu/50x50?text=n&fg=fff&bg=333';
-
 class PersonProduct extends StatelessWidget {
+  final String vueUrl = 'http://iph.href.lu/50x50?text=v&fg=fff&bg=000';
+  final String nodeUrl = 'http://iph.href.lu/50x50?text=n&fg=fff&bg=333';
+  final String _url = "http://baidu.com";
+
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,10 +38,19 @@ class PersonProduct extends StatelessWidget {
               subtitle: Text('一个node天气预报cli工具 '),
               trailing: Icon(Icons.keyboard_arrow_right),
               onTap: () {
+                print('test');
                 WebView(
-                    initialUrl: "http://baidu.com",
-                    //JS执行模式 是否允许JS执行
-                    javascriptMode: JavascriptMode.unrestricted);
+                    // initialUrl: _url,
+                    // //JS执行模式 是否允许JS执行
+                    // javascriptMode: JavascriptMode.unrestricted,
+                    // onWebViewCreated: (WebViewController webViewController) {
+                    //   _controller.complete(webViewController);
+                    // },
+                    // navigationDelegate: (NavigationRequest request) {
+                    //   // 判断URL
+                    //   return NavigationDecision.navigate;
+                    // },
+                    );
               },
             ),
           ],
